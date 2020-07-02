19c3b9ceb6c8c920397c85300c9e62428caac23e
Tri Yulianto <59768244+jujulili1@users.noreply.github.com>
2020-07-02 14:12:34 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormRegister from './pages/LandingPages/LandingPage'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormRegister />
+    </div>
+  );
+}
+
+export default App;
---
+++ LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormRegister from '../../components/FormRegister/FormRegister'
+function LandingPage() {
+    return (
+        <div>
+            <h1>Form Register</h1>
+            <FormRegister />
+        </div>
+    )
+}
+
+export default LandingPage
